e34d250c319a62052beae361fa5809bd2c2444fc
Nasser Benoudi <n.benoudi@gmail.com>
2020-10-31 22:40:36 +0100

    Updates

---
+++ precache-manifest.02facbd669235fdcc8557c60c72999fd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ffbf0e9bec1669add6ecd1daf05a10c5",
+    "url": "/nbenoudi/nbenoudi.github.io/index.html"
+  },
+  {
+    "revision": "20abc20e20c6c05006fa",
+    "url": "/nbenoudi/nbenoudi.github.io/static/css/main.28899bb6.chunk.css"
+  },
+  {
+    "revision": "3b05969637e67c94628e",
+    "url": "/nbenoudi/nbenoudi.github.io/static/js/2.b31cd100.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/nbenoudi/nbenoudi.github.io/static/js/2.b31cd100.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "20abc20e20c6c05006fa",
+    "url": "/nbenoudi/nbenoudi.github.io/static/js/main.5aa9e3a6.chunk.js"
+  },
+  {
+    "revision": "f305d434feda30973e55",
+    "url": "/nbenoudi/nbenoudi.github.io/static/js/runtime-main.2b876221.js"
+  }
+]);
